1bb69d02b94c1e765e6b3a81d2a018c244949f30
James Crocker <jbcrocker@pacbell.net>
2020-03-10 00:58:48 -0700

    made generic AST nodes generic and expandable

--- grammar/syntax_checker.js
+++ grammar/syntax_checker.js
@@ -31,7 +31,8 @@ function grammarEntryExpander(category){
 }
 function produceAST(filename){
     let source = fs.readFileSync(filename,"utf8")
-    let grammar = context2grammar(macroparser(filename))
+    let context = macroparser(filename)
+    let grammar = context2grammar(context)
     let match = grammar.match(tokenize_indents(source))
         
     if(match.failed()){
@@ -48,223 +49,148 @@ function produceAST(filename){
         }
         return null
     }
-    let astBuilder = grammar.createSemantics().addOperation('ast',defaultASTBuilder)
+    let nodeSpec = {}
+    Object.assign(nodeSpec,defaultNodeSpecs)
+    if(context.nodeSpec){
+        Object.assign(nodeSpec,context.nodeSpec)
+    }
+    
+    function node(type,...args){
+        return nodes[type].constructor(...args)
+    }
+
+    let nodes = {}
+    for (const [name,args] of Object.entries(defaultNodeSpecs)){
+        nodes[name]={}
+        nodes[name].template = {}
+        Object.assign(nodes[name].template,baseNodeTemplate)
+        nodes[name].template.type = name
+        nodes[name].constructor = function(){
+            let obj = {}
+            Object.assign(obj,nodes[name].template)
+            args.map((x,i)=>{obj[x] = arguments[i]})
+            return obj
+        }
+    }
+
+    const defaultASTBuilder = {
+        Specification(_1,head,_2,tail,_3){
+            return node('Specification',[head, tail].map(x => x.ast()))
+        },
+        Statement(statement){return statement.ast()},
+        Statement_print(_1,exp){return node('PrintStatement',exp.ast())},
+        Statement_put(_1,exp){return node('PutStatement',exp.ast())},
+        AssignmentStatement_functionAssignment(pattern,_1,srg){
+            return node('SubRoutineAssignment',pattern.ast(),srg.ast())
+        },
+        AssignmentStatement_regularAssignment(pattern,_1,exp){
+            return node('AssignmentStatement',pattern.ast(),exp.ast())
+        },
+        Pattern(pelems){return node('Pattern',pelems.ast())},
+        PatternElement(atom){return node('AtomicPattern',atom.ast())},
+        PatternElement_patternTuple(contents){
+            return node('TuplePatter',contents.ast())
+        },
+        PatternElement_patternList(ltype,head,_1,tail){
+            return node('ListPattern',ltype,head,tail)
+        },
+        PatternElement_type(type,atom){return node('TypePattern',type,atom)},
+        Type(id){return node('Type',id)},
+        Type_functionType(tin,_1,tout){return node('FunctionType',tin,tout)},
+        Type_list(lt){return lt.ast()},
+        ListType(_1,type,_2){return node('ListType',type)},
+        SubRoutineGroup(srgblock){
+            return node('SubRoutineGroup',srgblock.ast())
+        },
+        SubRoutineGroup_singular(stmtblock){
+            return node('SingularSubRoutineGroup',stmtblock.ast())
+        },
+        Routine(sr){return sr.ast()},
+        Function(_1,_2,srg){
+            return node('FunctionGroup',srg.ast())
+        },
+        Process(_1,_2,srg){
+            return node('ProcessGroup',srg.ast())
+        },
+        Server(_1,_2,srg){
+            return node('ServerGroup',srg.ast())
+        },
+        Guard(_1,exp){node('Guard',exp)},
+        Expression_newline(atoms,kwargblock){
+            return node('Expression',atoms,kwargblock)
+        },
+        Kwarg(id,_1,exp){return node('Kwarg',id,exp)},
+        Atom(contents){return contents.ast()},
+        Atom_singleton(_1,exp_block,_2){
+            return node('GroupedExpressions',exp_block.ast())
+        },
+        AbstractType(_1, _2, id, _3,_4,pattern_block){
+            return node('AbstractType',id.ast(),pattern_block.ast())
+        },
+        Select(object,_1,selection,_2){
+            return node('Select',object,selection)
+        },
+        Tuple(_1,neck,_2,rump,_3){
+            return node('Tuple',[...neck,rump].map(x => x.ast()) )
+        },
+        List_numlist(_1,start,_2,step,_3,end,_4){
+            return node('NumList',start.ast(),step.ast(),end.ast())
+        },
+        List(seq){
+            return node('List',seq.ast())
+        },
+        String(seq){
+            return node('StringNode',seq.ast())
+        },
+        id(contents){return node('Id',this.sourceString)},
+        numlit(_1,_2,_3,_4,_5,_6){return node('Numlit',this.sourceString)},
+        NonemptyListOf(head,sep,tail){return [head,tail].map(x=>x.ast())},
+    }
+
+    let ast_operations = {}
+    Object.assign(ast_operations,defaultASTBuilder)
+    if(context.ASTNodes){
+        Object.assign(ast_operations,context.ASTNodes)
+    }
+    let astBuilder = grammar.createSemantics().addOperation('ast',ast_operations)
     return astBuilder(match).ast()
 }
 
-function node(type,...args){
-    let newNode = new type
-    Object.keys(newNode).map((x,i) => newNode[x] = args[i])
-    return newNode
+const defaultNodeSpecs = {
+  Specification :["body"],
+  PrintStatement :["exp"],
+  PutStatement :["exp"],
+  SubRoutineAssignment :["id","subroutine"],
+  AssignmentStatement :["id","exp"],
+  Pattern :["patternElems"],
+  TuplePattern:["contents"],
+  ListPattern :["list_type","head","tail"],
+  TypePattern :["type","id"],
+  AtomicPattern :["id"],
+  FunctionType :["input_type","output_type"],
+  ListType:["type"],
+  Type:["id"],
+  SubRoutineGroup :["subroutine_block"],
+  SingularSubRoutineGroup:["statement_block"],
+  FunctionGroup:["subroutine_group"],
+  ProcessGroup:["subroutine_group"],
+  ServerGroup:["subroutine_group"],
+  Guard:["exp"],
+  Expression:["atoms","kwarg_block"],
+  Kwarg :["id","exp"],
+  GroupedExpressions:["exps"],
+  AbstractType:["id","pattern_block"],
+  Select:["subject","predicate"],
+  Tuple:["contents"],
+  NumList:["start","step","end"],
+  List:["contents"],
+  StringNode:["contents"],
+  MacroFlag:["id"],
+  Id:["id"],
+  Numlit:["num"],
 }
 
-class Specification {
-    body
-}
-
-class PrintStatement {
-    exp
-}
-
-class PutStatement {
-    exp
-}
-
-class SubRoutineAssignment {
-    id
-    subroutine
-}
-
-class AssignmentStatement {
-    id
-    exp
-}
-
-class Pattern {
-    patternElems
-}
-
-class TuplePattern {
-    contents
-}
-
-class ListPattern {
-    list_type
-    head
-    tail
-}
-
-class TypePattern {
-    type
-    id
-}
-
-class AtomicPattern {
-    id
-}
-
-class FunctionType {
-    input_type
-    output_type
-}
-
-class ListType{
-    type
-}
-
-class Type{
-    id
-}
-
-class SubRoutineGroup{
-    subroutine_block
-}
-
-class SingularSubRoutineGroup{
-    statement_block
-}
-
-class FunctionGroup{
-    subroutine_group
-}
-
-class ProcessGroup{
-    subroutine_group
-}
-
-class ServerGroup{
-    subroutine_group
-}
-
-class Guard{
-    exp
-}
-
-class Expression{
-    atoms
-    kwarg_block
-}
-
-class Kwarg {
-    id
-    exp
-}
-
-class GroupedExpressions{
-    exps
-}
-
-class AbstractType{
-    id
-    pattern_block
-}
-
-class Select{
-    subject
-    predicate
-}
-
-class Tuple{
-    contents
-}
-
-class NumList{
-    start
-    step
-    end
-}
-
-class List{
-    contents
-}
-
-class StringNode{
-    contents
-}
-
-class MacroFlag{
-    id
-}
-
-class Id{
-    id
-}
-
-class Numlit{
-    num
-}
-const defaultASTBuilder = {
-    Specification(_1,head,_2,tail,_3){
-        return node(Specification,[head, tail].map(x => x.ast()))
-    },
-    Statement(statement){return statement.ast()},
-    Statement_print(_1,exp){return node(PrintStatement,exp.ast())},
-    Statement_put(_1,exp){return node(PutStatement,exp.ast())},
-    AssignmentStatement_functionAssignment(pattern,_1,srg){
-        return node(SubRoutineAssignment,pattern.ast(),srg.ast())
-    },
-    AssignmentStatement_regularAssignment(pattern,_1,exp){
-        return node(AssignmentStatement,pattern.ast(),exp.ast())
-    },
-    Pattern(pelems){return node(Pattern,pelems.ast())},
-    PatternElement(atom){return node(AtomicPattern,atom.ast())},
-    PatternElement_patternTuple(contents){
-        return node(TuplePatter,contents.ast())
-    },
-    PatternElement_patternList(ltype,head,_1,tail){
-        return node(ListPattern,ltype,head,tail)
-    },
-    PatternElement_type(type,atom){return node(TypePattern,type,atom)},
-    Type(id){return node(Type,id)},
-    Type_functionType(tin,_1,tout){return node(FunctionType,tin,tout)},
-    Type_list(lt){return lt.ast()},
-    ListType(_1,type,_2){return node(ListType,type)},
-    SubRoutineGroup(srgblock){
-        return node(SubRoutineGroup,srgblock.ast())
-    },
-    SubRoutineGroup_singular(stmtblock){
-        return node(SingularSubRoutineGroup,stmtblock.ast())
-    },
-    Routine(sr){return sr.ast()},
-    Function(_1,_2,srg){
-        return node(FunctionGroup,srg.ast())
-    },
-    Process(_1,_2,srg){
-        return node(ProcessGroup,srg.ast())
-    },
-    Server(_1,_2,srg){
-        return node(ServerGroup,srg.ast())
-    },
-    Guard(_1,exp){node(Guard,exp)},
-    Expression_newline(atoms,kwargblock){
-        return node(Expression,atoms,kwargblock)
-    },
-    Kwarg(id,_1,exp){return node(Kwarg,id,exp)},
-    Atom(contents){return contents.ast()},
-    Atom_singleton(_1,exp_block,_2){
-        return node(GroupedExpressions,exp_block.ast())
-    },
-    AbstractType(_1, _2, id, _3,_4,pattern_block){
-        return node(AbstractType,id.ast(),pattern_block.ast())
-    },
-    Select(object,_1,selection,_2){
-        return node(Select,object,selection)
-    },
-    Tuple(_1,neck,_2,rump,_3){
-        return node(Tuple,[...neck,rump].map(x => x.ast()) )
-    },
-    List_numlist(_1,start,_2,step,_3,end,_4){
-        return node(NumList,start.ast(),step.ast(),end.ast())
-    },
-    List(seq){
-        return node(List,seq.ast())
-    },
-    String(seq){
-        return node(StringNode,seq.ast())
-    },
-    id(contents){return node(Id,this.sourceString)},
-    numlit(_1,_2,_3,_4,_5,_6){return node(Numlit,this.sourceString)},
-    NonemptyListOf(head,sep,tail){return [head,tail].map(x=>x.ast())},
+const baseNodeTemplate = {
 }
 
 if(!module.parent){

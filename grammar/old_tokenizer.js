ccbbf2a4c2f57e7e87cde1a3b7fabe1dac543db2
James Crocker <jbcrocker@pacbell.net>
2020-03-24 17:05:13 -0700

    old tokenizer

---
+++ grammar/old_tokenizer.js
@@ -0,0 +1,36 @@
+module.exports = tokenize_indents
+fs = require('fs')
+path = require('path')
+function tokenize_indents(source){
+    let [tokenized,_1] = source.split("\n").reduce((acc,x) => {
+        let [text,plen] = acc
+        let clen = get_indentation(x)
+        let y = x
+        if(clen !== null){
+            if(clen > plen){
+               y = "⇨" + y 
+            } else if(clen < plen){
+               y = "⇦" + y
+            }
+        }
+        return [text+"\n"+y,(clen !== null)? clen : plen ]
+    }  ,["",0])
+    return tokenized;
+}
+
+let leading_whitespace = /^\s+/
+let empty_line = /^(\s*|\n)$/
+function get_indentation(line){
+    if (empty_line.test(line)){
+        return null
+    }
+    let l = line.match(leading_whitespace)
+    if(l === null){
+        return 0
+    }
+    return l[0].length
+}
+
+if(!module.parent){
+    console.log(tokenize_indents(fs.readFileSync(path.resolve(__dirname,"../sample_programs/super_program.w"),'utf8')))
+}

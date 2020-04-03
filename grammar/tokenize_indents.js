050ef60a8d16a2dbce0ec5bf8cdbe6604581a351
benjaminjkern <bkern1@lion.lmu.edu>
2020-04-03 02:17:00 -0700

    added THOROUGH INDENTATION TESTS

--- grammar/tokenize_indents.js
+++ grammar/tokenize_indents.js
@@ -3,70 +3,54 @@ const path = require("path");
 module.exports = tokenize_indents;
 
 function tokenize_indents(source) {
-  let [tokenized, final_indents] = source.split("\n").reduce(
-    (acc, x) => {
-      let [text, plens] = acc;
-      let clen = get_indentation(x);
-      let y = x;
-      if (clen !== null) {
-        if (clen > plens.peek()) {
-          y = "\n" + "⇨" + "\n" + y;
-          plens.push(clen);
-        } else if (clen < plens.peek()) {
-          while (clen < plens.peek()) {
-            y = "\n" + "⇦" + "\n" + y;
-            plens.pop();
-          }
-        } else {
-          y = "\n" + y;
-        }
-      }
-      return [text + y, plens];
-    },
-    ["", stack([0])]
-  );
-  final_indents.pop(); // Get rid of initial 0
-  final_indents.map(x => (tokenized += "\n⇦\n"));
-  return tokenized;
-}
+    let indents = [0];
+    return source.split("\n").filter(line => get_indentation(line) !== null).map(line => {
+        let spaces = get_indentation(line);
+        let num_indents = indents.length - 1;
+        let current_indent = indents[num_indents];
+        if ([-1, current_indent].includes(spaces)) return line;
 
-function stack(x) {
-  x.peek = function() {
-    return this.slice(-1).pop();
-  };
-  return x;
+        if (spaces > current_indent) {
+            indents.push(spaces);
+            return "⇨\n" + line;
+        }
+
+        console.log(indents);
+        indents = indents.filter(num => num <= spaces)
+        console.log(indents);
+        return "⇦\n".repeat(num_indents - indents.length + 1) + line;
+    }).join("\n") + "\n" + "⇦\n".repeat(indents.length - 1);
 }
 
 let leading_whitespace = /^\s+/;
-let empty_line = /^(\s*|\s*\n)$/;
+let macro_line = /^\s*#!/
+let empty_line = /^(\s*|\s*#(?!\/|!).*)$/;
+
 function get_indentation(line) {
-  if (empty_line.test(line)) {
-    return null;
-  }
-  let l = line.match(leading_whitespace);
-  if (l === null) {
-    return 0;
-  }
-  return l[0].length;
+    if (macro_line.test(line)) return -1;
+    if (empty_line.test(line)) return null;
+    let l = line.match(leading_whitespace);
+    if (l === null) return 0;
+    return l[0].length;
 }
 
 if (!module.parent) {
-  const tokenizer = require(path.resolve(__dirname, "tokenize_indents.js"));
-  const ohm = require("ohm-js");
-  const basegrammar = ohm.grammar(
-    fs.readFileSync(path.resolve(__dirname, "cuttlefish.ohm"))
-  );
-  const tokenized = tokenize_indents(
-    fs.readFileSync(
-      path.resolve(__dirname, "../sample_programs/super_program.w"),
-      "utf8"
-    )
-  );
-  console.log(tokenized);
-  const match = basegrammar.match(tokenized);
-  if (match.failed()) {
-    console.log(match.message);
-  } else {
-    console.log("all dandy");
-  }
+    const tokenizer = require(path.resolve(__dirname, "tokenize_indents.js"));
+    const ohm = require("ohm-js");
+    const basegrammar = ohm.grammar(
+        fs.readFileSync(path.resolve(__dirname, "cuttlefish.ohm"))
+    );
+    const tokenized = tokenize_indents(
+        fs.readFileSync(
+            path.resolve(__dirname, "../sample_programs/super_program.w"),
+            "utf8"
+        )
+    );
+    console.log(tokenized);
+    const match = basegrammar.match(tokenized);
+    if (match.failed()) {
+        console.log(match.message);
+    } else {
+        console.log("all dandy");
+    }
 }

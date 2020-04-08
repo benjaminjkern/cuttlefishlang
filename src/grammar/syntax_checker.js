8c6bcdab1437637f1f7a2a58c2c094347e4c1dc8
Benjamin Kern <bkern1@lion.lmu.edu>
2020-04-08 16:01:14 -0700

    'fixed_it'

--- src/grammar/syntax_checker.js
+++ src/grammar/syntax_checker.js
@@ -3,4 +3,4 @@ const ohm = require('ohm-js');
 const baseGrammar = require('./base_grammar');
 const tokenizeIndents = require('./tokenize_indents');
 
-module.exports = text => baseGrammar.match(tokenizeIndents(text));
+module.exports = text => baseGrammar.match(tokenizeIndents(text.replace(/\r\n/g,'\n')));

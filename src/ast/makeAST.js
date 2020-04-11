f3d1dbeca340fd4572814d1df5e652f11e754102
James Crocker <jbcrocker@pacbell.net>
2020-04-11 15:13:11 -0700

    Added documentation to AST gen, and fixed some oddities

--- src/ast/makeAST.js
+++ src/ast/makeAST.js
@@ -10,7 +10,7 @@ const MacroHandler = require("../grammar/macro_parser.js");
 const tokenize_indents = require("../grammar/tokenize_indents");
 
 module.exports = (source) => {
-    let context = MacroHandler.macroparse(source);
+    let context = MacroHandler(source);
     let grammar = context2grammar(context);
     let match = grammar.match(tokenize_indents(source));
 
@@ -18,11 +18,10 @@ module.exports = (source) => {
         logTrace(grammar, match.message, source);
         return "";
     }
-
-    /*
-     * I only kinda get whats going on here
-     *  |
-     *  V
+    
+    /* 
+     * Creates the Node Specifications, by pulling any made by macros and combining them with the default ones.
+     * Node Specifications are a name of an AST node keyed to an array of fields in that node.
      */
 
     let nodeSpecs = {};
@@ -30,12 +29,27 @@ module.exports = (source) => {
     if (context.nodeSpecs) Object.assign(nodeSpecs, context.nodeSpecs);
     context.nodeSpecs = nodeSpecs;
 
+    /*
+     * Creates a parent (prototype) NodeTemplate (by pulling any made by macros and combining them with the default one.)
+     * A Node Template is the prototype of the AST Node objects.
+     */
+
     let baseNodeTemplate = { toString: defaultToString };
     Object.assign(baseNodeTemplate, defaultBaseNodeTemplate);
     if (context.baseNodeTemplate)
         Object.assign(baseNodeTemplate, context.baseNodeTemplate);
     context.baseNodeTemplate = baseNodeTemplate;
+    
+    /*
+     * This puts the default methods (toString and the like) into the parent NodeTemplate
+     */
     Object.assign(context.baseNodeTemplate, defaultMethods.default);
+
+    /*
+     * This adds the nodeConstructor for each AST Node type to the context.
+     * nodeConstructors include the template (protype) of that Node type,
+     * and the function that constructs Nodes of that type.
+     */
 
     context.nodeConstructors = {};
     for (const [name, args] of Object.entries(context.nodeSpecs)) {
@@ -53,11 +67,19 @@ module.exports = (source) => {
             return obj;
         };
     }
+
+    /*
+     * This adds methods to various Node templates, as defined in defaultMethods.
+     * Primarily used to add a terminal flag to terminal nodes.
+     */
     for (const [name, contents] of Object.entries(defaultMethods)) {
         if (name !== "default")
             Object.assign(context.nodeConstructors[name].template, contents);
     }
-
+    
+    /*
+     * The dynamic constructor of nodes. Just looks up the Node type's constructor and uses it.
+     */
     const node = (type, ...args) =>
         context.nodeConstructors[type].construct(...args);
 
@@ -70,11 +92,9 @@ module.exports = (source) => {
             node("AssignmentStatement", pattern.ast(), exp.ast()),
         Statement_print: (_1, exp) => node("PrintStatement", exp.ast()),
         Statement_put: (_1, exp) => node("PutStatement", exp.ast()),
-        Block_small: (list, _1, _2) => node("SmallBlock", list.ast()),
+        Block_small: (list, _1, _2) => list.ast(),
         Block_large: (_1, _2, _3, head, _4, _5, tail, _6, _7, _8, _9) =>
-            node(
-                "LargeBlock", [head, tail].map((x) => x.ast())
-            ),
+                [head, tail].map((x) => x.ast()),
         Pattern: (list) => node("Pattern", list.ast()),
         PatternElement: (atom) => node("AtomicPattern", atom.ast()),
         PatternElement_tuple: (_1, contents, _2) =>
@@ -82,7 +102,7 @@ module.exports = (source) => {
         PatternElement_list: (list_type, head, _1, tail) =>
             node("ListPattern", list_type.ast(), head.ast(), tail.ast()),
         PatternElement_typed: (type, patternElem) =>
-            node("TypedPattern", type.ast(), patternElem.ast()),
+            node("TypePattern", type.ast(), patternElem.ast()),
         Type: (id) => node("Type", id.ast()),
         Type_tuple: (_1, block, _2) => node("TupleType", block.ast()),
         Type_function: (type_in, _1, type_out) =>
@@ -104,10 +124,10 @@ module.exports = (source) => {
         Expression: (atom_list) =>
             node(
                 "Expression",
-                atom_list.map((x) => x.ast())
+                atom_list.ast()
             ),
         Atom: (contents) => contents.ast(),
-        Atom_singleton: (_1, exp, _2) => node("GroupedExpression", exp.ast()),
+        Atom_singleton: (_1, exp, _2) => node("Expression", exp.ast()),
         AbstractType: (type, _1, _2, block) =>
             node("AbstractType", type.ast(), block.ast()),
         DataPattern: (pattern, _1, block) =>
@@ -116,29 +136,39 @@ module.exports = (source) => {
         Selector: (list) => list.ast(),
         Selector_nonSlice: (_1, selectOp, _2) => selectOp.ast(),
         Tuple_nonempty: (_1, block, _2) => node("Tuple", block.ast()),
+        Tuple_empty: (_1,_2) => node("Tuple",[]),
         List_num: (_1, start, _2, step, _3, end, _4) =>
             node("NumList", start.ast(), step.ast(), end.ast()),
-        List_nonempty: (seq) => node("List", seq.ast()),
+        List_nonempty: (_1,seq,_2) => node("List", seq.ast()),
+        List_empty : (_1,_2) => node("List", []),
         index: (content) => content.ast(),
-        String: (seq) => node("String", this.sourceString),
+        String(seq){return node("String", this.sourceString)},
         String_interpolated: (_1, seq_head, _5, exp, _6, seq_tail, _10) =>
             node(
                 "InterpolatedString", [seq_head, seq_tail].map((x) => x.ast()),
                 exp.map((x) => x.ast())
             ),
-        stringbit: (_1, _2, _3) => node("StringLiteral", this.sourceString),
-        numlit: (_1, _2, _3, _4, _5, _6) =>
-            node("NumberLiteral", this.sourceString),
-        id: (contents) => node("Id", this.sourceString),
+        stringbit(_3){return node("String", this.sourceString)},
+        numlit(_1, _2, _3, _4, _5, _6){
+            return node("Numlit", this.sourceString)},
+        id(contents){return node("Id", this.sourceString)},
         NonemptyListOf: (head, sep, tail) => [head, tail].map((x) => x.ast()),
-        nospaceops: (op) => node("Operator", this.sourceString),
+        nospaceops(op){return node("Operator", this.sourceString)},
     };
+
+    /* 
+     * This adds macro-defined ASTOperations to the ast operation object
+     */
     let ast_operations = {};
     Object.assign(ast_operations, defaultASTOperations);
     if (context.ast_operations) {
         Object.assign(ast_operations, context.ast_operations);
     }
     context.ast_operations = ast_operations;
+
+    /*
+     * Builds the AST.
+     */
 
     let astBuilder = grammar
         .createSemantics()
@@ -234,11 +264,13 @@ const defaultNodeSpecs = {
     Numlit: ["num"],
     SubRoutine: ["pattern", "guard", "statements"],
     Operator: ["op"],
+    String: ["contents"],
+    InterpolatedString: ["strings","expressions"],
 };
 const defaultBaseNodeTemplate = {};
 
 function defaultToString() {
-    return util.inspect(this, false, null, true);
+    return util.inspect(this, false, null, false);
 }
 
 function inspector(depth, options) {
@@ -261,13 +293,13 @@ const defaultMethods = {
     Numlit: {
         terminal: true,
     },
-    StringNode: {
+    String: {
         terminal: true,
     },
 };
 
 function astArrayCleaner(ast) {
-    if (ast === undefined || ast.terminal) {
+    if (ast === undefined || ast === null || ast.terminal) {
         return;
     }
     for (const [key, val] of Object.entries(ast.fields)) {
